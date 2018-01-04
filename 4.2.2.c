62a8e938c7d0478c173de492235ab8c964d26235
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2018-01-04 14:02:32 +0100

    4.2.2.c

---
+++ 4.2.2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+# define ROZMIAR 4
+
+
+double na_tablicach_srednia (unsigned int n, int *tab );
+
+
+double na_tablicach_srednia (unsigned int n, int *tab )
+{
+
+    int wynik =0;
+
+    for ( int i=0;i < n;i++)
+
+    {
+
+       wynik +=tab[i];
+
+    }
+
+    double srednia = (double)wynik/n;
+
+    return srednia;
+
+}
+
+int main()
+
+{
+
+    int tablica[ROZMIAR]={1,2,3,4};
+
+    double srednia = na_tablicach_srednia (ROZMIAR, tablica);
+
+    printf("tablica ");
+
+    for(int i=0;i<ROZMIAR; i++)
+    {
+
+        printf("%d ",tablica[i]);
+
+    }
+
+    printf("\nSrednia arytmetyczna tej tablicy wynosi %.1f", srednia);
+
+    return 0;
+}
